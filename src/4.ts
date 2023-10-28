92f667223885b6b015a3bb6247834ebf05a9cea5
VladM <vdstories20@gmail.com>
2023-10-28 20:14:51 +0200

    fixed tasks

--- src/4.ts
+++ src/4.ts
@@ -13,31 +13,27 @@ class Key {
 const key = new Key();
 
 abstract class House {
-  door: false;
-  key: Key;
-  tenants: Array<Person> = [];
+    door: boolean = false;
+    key: Key;
+    tenants: Array<Person> = [];
 
-  comeIn(person: Person) {
-    if (!this.door) {
-      console.log("Door is open");
-        this.tenants.push(person);
-        this.door;
+    constructor(key: Key) {
+        this.key = key;
     }
-  }
+
+    comeIn(person: Person) {
+        if (!this.door) {
+            this.tenants.push(person);
+        }
+    }
 
   abstract openDoor(key: Key): void;
 }
 
 class MyHouse extends House {
-  constructor(key: Key) {
-    super();
-    this.key = key;
-  }
-
-  openDoor(key: Key):void {
+openDoor(key: Key):void {
     if (key.getSignature() === this.key.getSignature()) {
-      console.log("Welcome home!");
-      this.door;
+      this.door = true;
     }
   }
 }
